810461a820541ba1c53eabb2dfc3857ecfcced47
Nichlas <nichlasandre@users.noreply.github.com>
2017-05-28 17:49:24 +0200

    Commit update for SpriteSheet in JavaGUI

--- GUI Basics/TileGame/src/dev/nichlasandre/tilegame/gfx/Assets.java
+++ GUI Basics/TileGame/src/dev/nichlasandre/tilegame/gfx/Assets.java
@@ -1,5 +1,14 @@
 package dev.nichlasandre.tilegame.gfx;
 
+import java.awt.image.BufferedImage;
+
 public class Assets {
+	
+	public static BufferedImage dirt, grass, stone, endstone, glowstone, brick, bedrock, log, dragon_egg;
+	
+	public static void init() {
+		SpriteSheet sheet = new SpriteSheet(ImageLoader.loadImage("/textures/sheet.jpg"));
+		
+	}
 
 }
